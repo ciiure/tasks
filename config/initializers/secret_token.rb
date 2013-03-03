c325b74a948b23965badcda25f1e268c02142751
ciiure <ciiure1@hotmail.com>
2013-03-03 01:38:46 +0000

    Recreated App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '95c6f512a1e36e174fbe2ba395fd36966c53f73165c163dbfeef3d0da1a3c980da2287bc488e7f8de852ae2d068197a1e677e520c441f3429a190ced1f4c2476'
